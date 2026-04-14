4486a128771fe0544ac09ce716d419527e45cc91
Svante <svante.kask@gmail.com>
2026-04-14 12:15:05 +0200

    Fixes for formating setup

--- app/setup-husky.js
+++ app/setup-husky.js
@@ -1,5 +1,10 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-execSync('npx husky init', { stdio: 'inherit' });
+if (!fs.existsSync('.husky')) {
+  fs.mkdirSync('.husky');
+}
+
 fs.writeFileSync('.husky/pre-commit', 'npx lint-staged\n');
+
+execSync('npx husky', { stdio: 'inherit' });
