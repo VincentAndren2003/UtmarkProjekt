83f1537cdd1a075b502dc906722e4f0ea6f1aecf
VincentAndren2003 <vian1167@student.su.se>
2026-04-22 13:46:48 +0200

    quick fix for navigation to mapscreen

--- apps/mobile/src/screens/WelcomeScreen.tsx
+++ apps/mobile/src/screens/WelcomeScreen.tsx
@@ -1,12 +1,17 @@
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../App';
 
-export function WelcomeScreen() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Welcome'>;
+
+export function WelcomeScreen({ navigation }: Props) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>
         Welcome, nice you managed to create an account or logged in / Vincent
         was here!
       </Text>
+      <Button title="Go to Map" onPress={() => navigation.navigate('Map')} />
     </View>
   );
 }
