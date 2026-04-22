1fbc4cd90824f7849c89b1758b46638bba25be01
KascottGit <kasperscott@gmail.com>
2026-04-22 16:50:14 +0200

    Made route rendering and a test route

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,18 +1,21 @@
 import { HomeScreen } from './src/screens/HomeScreen';
 import { WelcomeScreen } from './src/screens/WelcomeScreen';
-import { MapScreen } from './src/screens/MapScreen';
+import { OrienteeringMapScreen } from './src/screens/OrienteeringMapScreen';
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { TEST_COURSE } from './src/testCourse';
 
 export default function App() {
   const [screen, setScreen] = useState<'home' | 'welcome'>('home');
+
+  const course = {}
 
   return (
     <>
       {screen === 'home' ? (
         <HomeScreen onGoWelcome={() => setScreen('welcome')} />
       ) : (
-        <MapScreen onBack={() => setScreen('home')}/>
+        <OrienteeringMapScreen onBack={() => setScreen('home') } courseGeoJSON={TEST_COURSE}/>
       )}
       <StatusBar style="auto" />
     </>
