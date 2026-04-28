64db1a3eac4e78402f3ce440b5004ef224f5c3fa
Svante <svante.kask@gmail.com>
2026-04-28 11:23:27 +0200

    Prettier formating

--- apps/mobile/src/screens/OrienteeringMapScreen.tsx
+++ apps/mobile/src/screens/OrienteeringMapScreen.tsx
@@ -1,7 +1,12 @@
 import { Button, StyleSheet, View, Text } from 'react-native';
-import React from "react";
-import { Camera, Map, GeoJSONSource, Layer, Images } from "@maplibre/maplibre-react-native";
-
+import React from 'react';
+import {
+  Camera,
+  Map,
+  GeoJSONSource,
+  Layer,
+  Images,
+} from '@maplibre/maplibre-react-native';
 
 type Props = {
   onBack: () => void;
@@ -9,7 +14,6 @@ type Props = {
 };
 
 export function OrienteeringMapScreen({ onBack, courseGeoJSON }: Props) {
-
   const ISOM_PURPLE = '#f94edf';
 
   const mapTilerKey = process.env.EXPO_PUBLIC_MAPTILER_KEY;
@@ -27,102 +31,95 @@ export function OrienteeringMapScreen({ onBack, courseGeoJSON }: Props) {
   return (
     <View style={styles.container}>
       <View style={styles.mapContainer}>
-          <Map 
-            mapStyle={styleURL}
-            contentInset={{ top: 16, right: 16, bottom: 16, left: 16 }}  
-            
-          >
-            <Camera 
-              zoom={10} 
-              center={[ 18.0656, 59.3327 ]} //Koordinater för stockholm (hårdkodad test)
+        <Map
+          mapStyle={styleURL}
+          contentInset={{ top: 16, right: 16, bottom: 16, left: 16 }}
+        >
+          <Camera
+            zoom={10}
+            center={[18.0656, 59.3327]} //Koordinater för stockholm (hårdkodad test)
+          />
+          <GeoJSONSource id="course-source" data={courseGeoJSON}>
+            {/* -- Connecting Line -- */}
+            <Layer
+              type="line"
+              id="course-line"
+              filter={['==', ['get', 'type'], 'route-line']}
+              style={{
+                lineColor: ISOM_PURPLE,
+                lineWidth: 2,
+                lineJoin: 'round',
+              }}
             />
-      <GeoJSONSource id="course-source" data={courseGeoJSON}>
-        
-        {/* -- Connecting Line -- */}
-        <Layer
-          type='line'
-          id="course-line"
-          filter={['==', ['get', 'type'], 'route-line']}
-          style={{
-            lineColor: ISOM_PURPLE,
-            lineWidth: 2,
-            lineJoin: 'round',
-          }}
-        />
 
-        {/* -- Kontroller -- */}
-        <Layer
-          type='circle'
-          id="control-rings"
-          filter={['==', ['get', 'type'], 'control']}
-          style={{
-            circleRadius: 18,
-            circleColor: 'transparent',
-            circleStrokeWidth: 2,
-            circleStrokeColor: ISOM_PURPLE,
-          }}
-        />
-        <Layer
-          type='symbol'
-          id="control-numbers"
-          filter={['==', ['get', 'type'], 'control']}
-          style={{
-            textField: ['get', 'number'],
-            textSize: 22,
-            textColor: ISOM_PURPLE,
-            textAnchor: 'bottom-left',
-            textOffset: [.75, -.75],
-            textHaloColor: '#FFFFFF',
-            textHaloWidth: 1,
-          }}
-        />
+            {/* -- Kontroller -- */}
+            <Layer
+              type="circle"
+              id="control-rings"
+              filter={['==', ['get', 'type'], 'control']}
+              style={{
+                circleRadius: 18,
+                circleColor: 'transparent',
+                circleStrokeWidth: 2,
+                circleStrokeColor: ISOM_PURPLE,
+              }}
+            />
+            <Layer
+              type="symbol"
+              id="control-numbers"
+              filter={['==', ['get', 'type'], 'control']}
+              style={{
+                textField: ['get', 'number'],
+                textSize: 22,
+                textColor: ISOM_PURPLE,
+                textAnchor: 'bottom-left',
+                textOffset: [0.75, -0.75],
+                textHaloColor: '#FFFFFF',
+                textHaloWidth: 1,
+              }}
+            />
 
-        {/* -- Finnish -- */}
-        {/* Outer Circle */}
-        <Layer
-          type='circle'
+            {/* -- Finnish -- */}
+            {/* Outer Circle */}
+            <Layer
+              type="circle"
+              id="finish-outer"
+              filter={['==', ['get', 'type'], 'finish']}
+              style={{
+                circleRadius: 18,
+                circleColor: 'transparent',
+                circleStrokeWidth: 2,
+                circleStrokeColor: ISOM_PURPLE,
+              }}
+            />
+            {/* Inner Circle */}
+            <Layer
+              type="circle"
+              id="finish-inner"
+              filter={['==', ['get', 'type'], 'finish']}
+              style={{
+                circleRadius: 12,
+                circleColor: 'transparent',
+                circleStrokeWidth: 2,
+                circleStrokeColor: ISOM_PURPLE,
+              }}
+            />
 
-          id="finish-outer"
-          filter={['==', ['get', 'type'], 'finish']}
-          style={{
-            circleRadius: 18,
-            circleColor: 'transparent',
-            circleStrokeWidth: 2,
-            circleStrokeColor: ISOM_PURPLE,
-          }}
-        />
-        {/* Inner Circle */}
-        <Layer
-          type='circle'
-
-          id="finish-inner"
-          filter={['==', ['get', 'type'], 'finish']}
-          style={{
-            circleRadius: 12,
-            circleColor: 'transparent',
-            circleStrokeWidth: 2,
-            circleStrokeColor: ISOM_PURPLE,
-          }}
-        />
-
-        {/* -- Start -- */}
-        <Layer
-          type='symbol'
-          id="start-triangle-layer"
-          filter={['==', ['get', 'type'], 'start']}
-          style={{
-            textField: '▲',
-            textSize: 32,
-            textColor: 'transparent',
-            textHaloColor: ISOM_PURPLE,
-            textHaloWidth: 2,
-          }}
-        />
-        
-
-      </GeoJSONSource>
-
-          </Map>
+            {/* -- Start -- */}
+            <Layer
+              type="symbol"
+              id="start-triangle-layer"
+              filter={['==', ['get', 'type'], 'start']}
+              style={{
+                textField: '▲',
+                textSize: 32,
+                textColor: 'transparent',
+                textHaloColor: ISOM_PURPLE,
+                textHaloWidth: 2,
+              }}
+            />
+          </GeoJSONSource>
+        </Map>
       </View>
       <View style={styles.menu}>
         <Button title="Back" onPress={onBack} />
@@ -137,12 +134,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  mapContainer:{
+  mapContainer: {
     width: '100%',
-    height: '100%'
+    height: '100%',
   },
-  menu:{
+  menu: {
     bottom: 10,
-    position: 'absolute'
+    position: 'absolute',
   },
 });
--- apps/mobile/src/testCourse.ts
+++ apps/mobile/src/testCourse.ts
@@ -1,42 +1,54 @@
 export const TEST_COURSE = {
-  type: "FeatureCollection",
+  type: 'FeatureCollection',
   features: [
     {
-      type: "Feature",
+      type: 'Feature',
       geometry: {
-        type: "LineString",
+        type: 'LineString',
         coordinates: [
           [18.161345832564905, 59.28992782384871], // Start
           [18.167132635232818, 59.289336641852344], // Control 1
           [18.159692935372995, 59.2862579140567], // Control 2
-          [18.156246529926673, 59.29060978989479]  // Finish
-        ]
+          [18.156246529926673, 59.29060978989479], // Finish
+        ],
       },
-      properties: { type: "route-line" }
+      properties: { type: 'route-line' },
     },
     // Start
     {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [18.161345832564905, 59.28992782384871] },
-      properties: { type: "start" }
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [18.161345832564905, 59.28992782384871],
+      },
+      properties: { type: 'start' },
     },
     // Control 1
     {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [18.167132635232818, 59.289336641852344] },
-      properties: { type: "control", number: "1" }
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [18.167132635232818, 59.289336641852344],
+      },
+      properties: { type: 'control', number: '1' },
     },
     // Control 2
     {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [18.159692935372995, 59.2862579140567] },
-      properties: { type: "control", number: "2" }
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [18.159692935372995, 59.2862579140567],
+      },
+      properties: { type: 'control', number: '2' },
     },
     // The Finish
     {
-      type: "Feature",
-      geometry: { type: "Point", coordinates: [18.156246529926673, 59.29060978989479] },
-      properties: { type: "finish" }
-    }
-  ]
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [18.156246529926673, 59.29060978989479],
+      },
+      properties: { type: 'finish' },
+    },
+  ],
 };
