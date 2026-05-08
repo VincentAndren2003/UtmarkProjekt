35d63e58151f9525a67af6032d80f9c634065f64
VincentAndren2003 <vian1167@student.su.se>
2026-05-08 23:35:52 +0200

    Fix ecosystemconfig patway

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -1,28 +1,34 @@
+const path = require('path');
+
+const appsApi = __dirname;
+const appsAuth = path.join(__dirname, '..', 'auth-service');
+const appsProfile = path.join(__dirname, '..', 'profile-service');
+
 module.exports = {
   apps: [
     {
       name: 'api',
-      cwd: 'apps/api',
-      script: 'dist/server.js',
-      env_file: '.env',
+      cwd: appsApi,
+      script: path.join(appsApi, 'dist', 'server.js'),
+      env_file: path.join(appsApi, '.env'),
       env: {
         PORT: 3000,
       },
     },
     {
       name: 'auth-service',
-      cwd: 'apps/auth-service',
-      script: 'dist/server.js',
-      env_file: '.env',
+      cwd: appsAuth,
+      script: path.join(appsAuth, 'dist', 'server.js'),
+      env_file: path.join(appsAuth, '.env'),
       env: {
         PORT: 3001,
       },
     },
     {
       name: 'profile-service',
-      cwd: 'apps/profile-service',
-      script: 'dist/server.js',
-      env_file: '.env',
+      cwd: appsProfile,
+      script: path.join(appsProfile, 'dist', 'server.js'),
+      env_file: path.join(appsProfile, '.env'),
       env: {
         PORT: 3002,
       },
