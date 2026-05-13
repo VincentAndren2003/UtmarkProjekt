fbbe537f96356342fe7d806576b142935d235d5f
Svante <svante.kask@gmail.com>
2026-05-13 14:01:43 +0200

    changes in env config

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -14,6 +14,7 @@ export const env = {
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? '7d',
   CORS_ORIGIN: process.env.CORS_ORIGIN ?? '*',
   AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL ?? 'http://127.0.0.1:3001',
-  PROFILE_SERVICE_URL:
-    process.env.PROFILE_SERVICE_URL ?? 'http://127.0.0.1:3002',
+  PROFILE_SERVICE_URL:process.env.PROFILE_SERVICE_URL ?? 'http://127.0.0.1:3002',
+  MONGODB_URI: required('MONGODB_URI'),
+  DB_NAME: process.env.DB_NAME ?? 'test',
 };
