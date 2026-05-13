c23765ad6b9b88fcb6d89b53ca3da87820e8d1d4
Svante <svante.kask@gmail.com>
2026-05-13 13:58:06 +0200

    Changes in server

--- apps/api/dist/server.js
+++ apps/api/dist/server.js
@@ -1,8 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 const env_1 = require("./config/env");
+const mongoose_1 = __importDefault(require("mongoose"));
 async function bootstrap() {
+    await mongoose_1.default.connect(process.env.MONGODB_URI, {
+        dbName: process.env.DB_NAME,
+    });
+    console.warn('Ansluten till MongoDB');
     const app = (0, app_1.createApp)();
     app.listen(env_1.env.PORT, () => {
         console.warn(`API listening on http://localhost:${env_1.env.PORT}`);
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,13 @@
 import { createApp } from './app';
 import { env } from './config/env';
+import mongoose from 'mongoose';
 
 async function bootstrap(): Promise<void> {
+  await mongoose.connect(process.env.MONGODB_URI as string, {
+    dbName: process.env.DB_NAME,
+  });
+  console.warn('Ansluten till MongoDB');
+
   const app = createApp();
   app.listen(env.PORT, () => {
     console.warn(`API listening on http://localhost:${env.PORT}`);
