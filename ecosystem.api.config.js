27377c930d4a02392c00d6df1b97c30ca6a43bf0
Svante <svante.kask@gmail.com>
2026-05-20 14:44:42 +0200

    udpated ecosystem config

--- ecosystem.api.config.js
+++ ecosystem.api.config.js
@@ -1,9 +1,11 @@
+const dotenv = require('dotenv');
+
 module.exports = {
   apps: [
     {
       name: 'api',
       script: './apps/api/dist/server.js',
-      env_file: './apps/api/.env',
+      env: dotenv.config({ path: './apps/api/.env' }).parsed,
     },
   ],
 };
